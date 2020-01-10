b83093eefb03df66cda5322421438c1d5adb1755
William Araujo <williamf1f2@gmail.com>
2020-01-10 11:07:18 -0300

    Acrescentar script de testes

---
+++ RandomTests.js
@@ -0,0 +1,35 @@
+const banco = {
+    conta: '0000 11111-2',
+    titular: "William",
+    saldo: 1000,
+    sacar: function (valor) {
+        banco.saldo -= valor;
+    },
+    depositar: function (valor) {
+        banco.saldo += valor;
+    }
+};
+
+console.log(banco.saldo);
+banco.sacar(100);
+
+console.log(banco.saldo);
+
+banco.depositar(200);
+console.log(banco.saldo);
+
+
+// console.log('1 - Saldo inicial: ', banco.saldo);
+// console.log('2 - Saldo depois do aumento: '+ banco.saldo + 2000);
+
+// // resolvendo o problema colocando virgula
+// console.log('3 - Saldo depois do aumento: ', banco.saldo + 2000);
+
+
+// // outro jeito de resolver o problema é criar uma variavel
+// let aumento = banco.saldo + 2000;
+
+// console.log('4 - Saldo depois do aumento :' + aumento);
+
+// // forma ideal de resolver tudo
+// console.log(`5 - Saldo depois do aumento: ${banco.saldo + 2000}`);
